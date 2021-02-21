091de46ec9d8f022ad4830674160e07273a9526e
tahaoyingjun <46505848+tahaoyingjun@users.noreply.github.com>
2021-02-21 01:37:19 -0800

    Tested, half of APIs has errors

--- Collimate/Database/DatabaseProtocol.swift
+++ Collimate/Database/DatabaseProtocol.swift
@@ -10,32 +10,32 @@ import Foundation
 protocol DatabaseManagerProtocal {
     
     // User level operations
-    func getUser(identifier email: String, completion: @escaping( (User?) -> Void ))
+    func getUser(identifier email: String, completion: @escaping( (User?) -> Void )) // somewhat thread kills
     
     func createUser(with user: User)
     
     func doesUserExists(identifier email: String, completion: @escaping( (Bool?) -> Void ))
     
-    func getUserChats(identifier email: String, completion: @escaping( ([Chat]?) -> Void ))
+    func getUserChats(identifier email: String, completion: @escaping( ([Chat]?) -> Void )) // somewhat thread kills
     
-    func joinChat(identifier email: String, chatId: String, completion: @escaping( (Bool) -> Void ))
+    func joinChat(identifier email: String, chatId: String, completion: @escaping( (Bool) -> Void )) // works well
     
-    func leaveChat(identifier email: String, chatId: String, completion: @escaping( (Bool) -> Void ))
+    func leaveChat(identifier email: String, chatId: String, completion: @escaping( (Bool) -> Void )) // works well
     
     // Chat operations
-    func getChat(chatId: String, completion: @escaping( (Chat?) -> Void ))
+    func getChat(chatId: String, completion: @escaping( (Chat?) -> Void )) // works well
     
-    func sendText(identifier email: String, chatId: String, text: String, completion: @escaping( (Bool) -> Void ))
+    func sendText(identifier email: String, chatId: String, text: String, completion: @escaping( (Bool) -> Void )) // sendText doesn't add message into chat
     
-    func getHistory(timestamp until: Date, chatId: String, completion: @escaping( ([Message]?) -> Void ))
+//    func getHistory(timestamp until: Date, chatId: String, completion: @escaping( ([Message]?) -> Void ))
+//
+//    func getHistory(numOfMessages until: Int, chatId: String, completion: @escaping( ([Message]?) -> Void ))
     
-    func getHistory(numOfMessages until: Int, chatId: String, completion: @escaping( ([Message]?) -> Void ))
+    func getUsersInChat(chatId: String, completion: @escaping( ([User]?) -> Void )) // works well
     
-    func getUsersInChat(chatId: String, completion: @escaping( ([User]?) -> Void ))
-    
-    func searchChat(search_token: String, completion: @escaping( ([Chat]?) -> Void ))
+    func searchChat(search_token: String, completion: @escaping( ([Chat]?) -> Void )) // bad results
     
     // Message operations
-    func getMessage(messageId: String, completion: @escaping( (Message?) -> Void ))
+    func getMessage(messageId: String, completion: @escaping( (Message?) -> Void )) // returend an error
     
 }
